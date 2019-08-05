0e806c407426ce7e5fdca55ddbf4f5ecb0bb7587
Shreyas Parthasarathy <shreyas.partha@berkeley.edu>
2019-08-05 15:31:17 -0700

    Officer Update fa2019

--- js/main.js
+++ js/main.js
@@ -46,31 +46,38 @@ angular
 .controller('membersctrl', function($scope) {
 	$scope.officers = [
 		{
-            bio: "Hi! My name is Shreyas and I'm a fifth-year Engineering Physics and EECS major. I've served as a TA for the CS department and love teaching and learning about anything and everything (hit me up with your latest random factoid!). In my free time I love listening to all kinds of music, practicing classical guitar or tenor saxophone, playing tennis, and cooking.",
+            bio: "My name is Anthony Vogliano, and I'm a fourth-year Engineering Physics major and EECS minor. I'm your run-of-the-mill physics geek, wannabe racquetball pro, and flannel enthusiast, and I'm excited to contribute to the SES community!",
 			description: "",
-			img: "img/shreyas.jpg",
-			name: "Shreyas Parthasarathy",
+			img: "img/anthony.jpg",
+			name: "Anthony Vogliano",
 			position: "President"
 		},
 		{
-            bio: "",
+            bio: "T.G. is currently a Junior at UC Berkeley majoring in Energy Engineering. She is the Vice President of Society of Engineering Sciences (SES) as well as the co-President of Engineers for a Sustainable World (ESW). She runs a project within ESW, called Man Made Floating Wetlands, which aims to remove the harmful algae on Lake Temescal in Oakland. She is part of another project through ESW called Rainwater Harvesting, that has built a rainwater harvesting system on the Hearst Field Annex in order to save drinking water. She loves reading and talking about energy related topics, and is very social so don't be afraid to email or reach out to her!",
 			description: "",
-			img: "",
-			name: "Mona Elalami",
+			img: "img/tg.jpg",
+			name: "T.G. Roberts",
 			position: "Vice-President"
 		},
 		{
-            bio: "My name is Anthony Vogliano, and I'm a third-year Engineering Physics major and EECS minor. I'm your run-of-the-mill physics geek, wannabe racquetball pro, and flannel enthusiast, and I'm excited to contribute to the SES community!",
+            bio: "Hello! My name is Daniel Frise, and I'm a junior majoring in Engineering Math and Statistics. I am pleased to be able to serve as the Secretary-Treasurer of the Society of Engineering Sciences. Previously I was the Publicity Chair of SES. Beyond this amazing organization, I am an active member of the Newman Hall Parish. In my free time, I enjoy reading books, writing stories, and playing video games (currently Overwatch). I look forward to serving the Engineering Science community for the 2019-2020 academic year!",
             description: "",
-            img: "img/anthony.jpg",
-            name: "Anthony Vogliano",
-			position: "Treasurer/Engineering Physics Representative"
+            img: "img/daniel.jpg",
+            name: "Daniel Frise",
+			position: "Secretary/Treasurer"
 		},
         {
             bio: "",
             description: "",
-            img: "",
-            name: "T.G. Roberts",
+            img: "img/nothing.jpg",
+            name: "Difan Wen",
+            position: "Engineering Physics Representative"
+        },
+        {
+            bio: "Brittany is an Energy Engineering Major at UC Berkeley, and is planning to graduate in the spring of 2021. In addition to her involvement with SES, Brittany is an active member of the Irish Dancers of Berkeley and Engineers for a Sustainable World, and is an officer in the engineering honors society Tau Beta Pi. She is passionate about environmental conservation, and hopes to pursue a career in clean, renewable energy generation.",
+            description: "",
+            img: "img/brittany.jpg",
+            name: "Brittany Wais",
             position: "Energy Engineering Representative"
         },
 		{
@@ -81,10 +88,10 @@ angular
 			position: "Engineering Mathematics & Statistics Representative"
 		},
 		{
-            bio: "Hey everyone! I'm Erina, a fourth-year Environmental Engineering Science major. You might find me running simulations for an Engineers Without Borders project or doing research, but more likely you'll just see me doing an impromptu song and dance in a random place on campus. I'm game for anything related to music, food, books, and baseball.",
+            bio: "Hi! I’m a sophomore majoring in Environmental Engineering Science with a minor in Marine Science. Ever since I was a young child, I’ve enjoyed watching National Geographic documentaries about the diverse ecosystems and organisms that inhabit Earth, sparking my interest in the environment. In my free time, I enjoy snowboarding, fishing, playing tennis, and maintaining my fresh and saltwater fish tanks. I'm ALWAYS free for a Jamba Juice run!",
 			description: "",
-			img: "img/erina.jpeg",
-			name: "Erina Szeto",
+			img: "img/derek.jpg",
+			name: "Derek Morimoto",
 			position: "Environmental Engineering Science Representative"
 		}
 	];
